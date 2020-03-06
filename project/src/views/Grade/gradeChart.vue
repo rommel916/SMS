567df6aae83eebaf1a8e61e3dc5a567582c91d69
“se <1411818691@qq.com>
2020-03-06 20:04:02 +0800

    修改二

--- project/src/views/Grade/gradeChart.vue
+++ project/src/views/Grade/gradeChart.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <Layout class="" :style="{marginLeft: '200px'}">
+      <Content :style="{padding: '0 16px 16px'}">
+        <Breadcrumb :style="{margin: '16px 0'}">
+          <BreadcrumbItem>成绩检测</BreadcrumbItem>
+          <BreadcrumbItem>成绩图表</BreadcrumbItem>
+        </Breadcrumb>
+          
+      </Content>
+    </Layout>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+ 
+    };
+  },
+  methods: {
+  
+  }
+};
+</script>
+<style lang="stylus" scoped></style>
